0e2e02a1cdd46659a9e9a0056dbaf5b984655451
Allegro33Najad <63730384+Allegro33Najad@users.noreply.github.com>
2023-03-28 15:14:07 +0300

    Add project files.

---
+++ Empower_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("DD816276-2E85-4625-BB66-613FFDCD52A3")]
+[assembly: AssemblyVersion("1.0.0.0")]
