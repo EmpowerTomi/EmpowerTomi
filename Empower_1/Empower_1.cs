82372c1b85f13b828bd72a795e28396fd80c53b2
Allegro33Najad <63730384+Allegro33Najad@users.noreply.github.com>
2023-03-28 17:09:37 +0300

    example use

--- Empower_1/Empower_1.cs
+++ Empower_1/Empower_1.cs
@@ -56,7 +56,8 @@ namespace Empower_1
 	using System.Globalization;
 	using System.Text;
 	using Skyline.DataMiner.Automation;
-	
+	using Skyline.DataMiner.Core.DataMinerSystem.Automation;
+
 	/// <summary>
 	/// Represents a DataMiner Automation script.
 	/// </summary>
@@ -68,7 +69,14 @@ namespace Empower_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Test");
+			var dms = engine.GetDms();
+
+			var elements = dms.GetElements();
+
+			foreach (var element in elements)
+			{
+				engine.GenerateInformation("element: " + element.Name);
+			}
 		}
 	}
 }
